832f8a4c2f0b052630e8213b064ed232f0296c4e
Frenny De La Cruz <102378124+frennysmith15@users.noreply.github.com>
2024-02-01 17:04:15 -0400

    Done

---
+++ DardosGame/Dardos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Dardos
+{
+	private int dado1;
+	private int dado2;
+	private int dado3;
+
+
+	public Dardos()
+	{
+		dado1 = Random.Next(1, 8);
+		dado2 = Random.Next(1, 8);
+		dado3 = Random.Next(1, 8);
+    }
+
+	public void Jugar()
+	{
+		if (dado1 == dado2 && dado2 == dado3)
+		{
+			Console.WriteLine("Ganaste, dado1 = " + dado1 + " dado2 = " + dado2 + "dado3 = " + dado3);
+		}
+		else
+		{
+            Console.WriteLine("Perdiste, dado1 = " + dado1 + " dado2 = " + dado2 + "dado3 = " + dado3);
+        }
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dardos dardos = new Dardos();
+
+		dardos.Jugar();
+
+    }
+}
+
+
